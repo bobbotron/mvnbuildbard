e039cdaac84d94607f5f001bdb1337d1b2bcf0de
Rob Young <ryoung@tresensa.com>
2020-12-13 11:11:09 -0500

    Added readme and comment

---
+++ src/main/java/README.md
@@ -0,0 +1,3 @@
+# JFugue library
+
+There is no entry for JFugue in the maven central repository.  This creates a library dependency headache for creating a maven listener.  The easiest way to get around this was to include the JFugue source in this project.  If someone could get JFugue listed on the central maven repository that would make this much cleaner!
--- src/main/java/com/rjysoft/mvn/test/maven/listener/MavenBardSpy.java
+++ src/main/java/com/rjysoft/mvn/test/maven/listener/MavenBardSpy.java
@@ -1,19 +1,15 @@
 package com.rjysoft.mvn.test.maven.listener;
 
 import org.apache.maven.eventspy.EventSpy;
-import org.apache.maven.execution.ExecutionEvent;
 import org.apache.maven.execution.MavenExecutionResult;
 import org.codehaus.plexus.component.annotations.Component;
-import org.jfugue.pattern.Pattern;
-import org.jfugue.player.Player;
-import org.jfugue.theory.ChordProgression;
 
 /**
  * This maven event spy listens for key maven events and links them to JFugue
  * notes
  */
 @Component(role = EventSpy.class, hint = "mvnbuildbardspy")
-public class MidiPlayerSpy extends org.apache.maven.eventspy.AbstractEventSpy
+public class MavenBardSpy extends org.apache.maven.eventspy.AbstractEventSpy
 {
 
 	private final BardPlayer player = new BardPlayer();
@@ -29,6 +25,10 @@ public class MidiPlayerSpy extends org.apache.maven.eventspy.AbstractEventSpy
 	
 		player.play(generator.generatePattern(event));
 		
+		/** 
+		 * The bard player will get cut off if we don't sleep for a little bit 
+		 * at the end of the project
+		 */
 		if (event instanceof MavenExecutionResult)
 		{
 			Thread.sleep(3000);
